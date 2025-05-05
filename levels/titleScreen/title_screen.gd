790e9716d0115cb29a277fb9c3cb7dc8f4d33a61
JoshuaW13 <josh.walters@shaw.ca>
2025-05-05 12:29:48 -0600

    Made basic desert map

--- levels/titleScreen/title_screen.gd
+++ levels/titleScreen/title_screen.gd
@@ -1,4 +1,4 @@
 extends Control
 
 func _on_button_button_up() -> void:
-	get_tree().change_scene_to_file("res://levels/field/field.tscn")
+	get_tree().change_scene_to_file("res://levels/desert/desert.tscn")
