b772ae267cac28e31d2ebf6da42fb5d73b895bc9
JoshuaW13 <josh.walters@shaw.ca>
2025-05-21 15:25:27 -0600

    Inventory swap swaps out player's item

--- entities/player/inventory/inventory.gd
+++ entities/player/inventory/inventory.gd
@@ -10,17 +10,17 @@ signal selected_item_changed(item: InventoryItem, index: int)
 var items: Array[InventoryItem] = []
 var selected_item_index: int = -1:
 	set(inventory_slot):
-		if inventory_slot >=MIN_SLOT and inventory_slot <= MAX_SLOT:
-			selected_item_index = inventory_slot
-			selected_item = items[inventory_slot]
-		else:
-			print("bad inventory swap!");
+		if inventory_slot != selected_item_index:
+			if inventory_slot >=MIN_SLOT and inventory_slot <= MAX_SLOT:
+				selected_item_index = inventory_slot
+				selected_item = items[inventory_slot]
+			else:
+				print("bad inventory swap!");
 
 var selected_item: InventoryItem = null:
 	set(item):
 		selected_item_changed.emit(item, selected_item_index)
 		selected_item = item
-
 
 func _ready() -> void:
 	await owner.ready
@@ -36,8 +36,14 @@ func add_item(item: InventoryItem)->void:
 				existing_item.count += item.count
 				return
 	elif not has_item(item.name):
-		items.append(item)
+		add_new_item(item)
 		item_added.emit(item)
+
+func add_new_item(item: InventoryItem)->void:
+	for i in items.size():
+		if items[i] ==null:
+			items[i] = item
+			break
 
 func has_item(item_name: String) -> bool:
 	for i in items:
--- entities/player/player.gd
+++ entities/player/player.gd
@@ -4,12 +4,12 @@ class_name Player
 @onready var velocity_component: VelocityComponent = $VelocityComponent
 @onready var player_direction_controller: FlipOnMouseComponent = $FlipOnMouseComponent
 @onready var weapon_direction_controller: RotateOnMouseComponent = %RotateOnMouseComponent
-@onready var weapon: Gun = %Pistol
 @onready var flip_pivot: Node2D = $Pivot
 @onready var camera: Camera2D = $Camera2D
 @onready var inventory: Inventory = %Inventory
 @onready var inventory_bar_ui: InventoryBar = %InventoryBar
 
+var weapon: Gun = null
 var primary_action_held: bool = false
 
 func _ready() -> void:
@@ -19,8 +19,8 @@ func _ready() -> void:
 	SignalBus.level_limits.connect(on_level_limits)
 	inventory_bar_ui.inventory = inventory
 
-func _process(delta: float) -> void:
-	if primary_action_held:
+func _process(_delta: float) -> void:
+	if primary_action_held and weapon:
 		weapon.shoot()
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -31,9 +31,18 @@ func _unhandled_input(event: InputEvent) -> void:
 
 func _on_health_component_health_depleted() -> void:
 	get_tree().call_deferred("change_scene_to_file", "res://levels/titleScreen/titleScreen.tscn")
-	
+
 func on_level_limits(left_limit:int, right_limit:int, top_limit:int, bottom_limit:int)->void:
 	camera.set_limit(SIDE_LEFT,left_limit)
 	camera.set_limit(SIDE_RIGHT,right_limit)
 	camera.set_limit(SIDE_TOP,top_limit)
 	camera.set_limit(SIDE_BOTTOM,bottom_limit)
+
+func _on_inventory_selected_item_changed(item: InventoryItem, _index: int) -> void:
+	if weapon:
+		weapon.queue_free()
+		weapon = null
+	if item:
+		weapon = item.scene_data.instantiate()
+		flip_pivot.add_child(weapon)
+		weapon_direction_controller.patient = weapon
