a5531ab35f21901f6fa47baccf607fe47681d0a6
JoshuaW13 <josh.walters@shaw.ca>
2025-05-25 11:38:40 -0600

    Created pointSpawner component to spawn wood

---
+++ common/components/pointSpawner/pointSpawner.gd
@@ -0,0 +1,15 @@
+extends Node2D
+class_name PointSpawner
+
+var origin : Vector2 = Vector2(0,0)
+var max_distance: float = 0.0
+
+func spawn(scene: PackedScene) -> void:
+	var scene_instance: Node2D = scene.instantiate()
+
+	var angle: float = randf() * TAU
+	var distance_from_origin: float = randf() * max_distance
+	var offset: Vector2 = Vector2(cos(angle), sin(angle)) * distance_from_origin
+	
+	scene_instance.position = origin + offset
+	get_tree().current_scene.add_child(scene_instance)
--- entities/objects/tree/tree.gd
+++ entities/objects/tree/tree.gd
@@ -2,9 +2,13 @@ extends StaticBody2D
 
 @export var wood_scene: PackedScene = load("res://entities/objects/wood/wood.tscn")
 
+@onready var point_spawner: PointSpawner = %PointSpawner
+
+func _ready() -> void:
+	point_spawner.origin = position
+	point_spawner.max_distance = 50
+
 func _on_health_component_health_depleted() -> void:
 	for i in 3:
-		var new_wood: Wood = wood_scene.instantiate()
-		new_wood.position = position
-		get_tree().current_scene.add_child(new_wood)
+		point_spawner.spawn(wood_scene)
 	queue_free()
