ca2e81bcc9f68d1e13131165cb138a29e18323e9
JoshuaW13 <josh.walters@shaw.ca>
2025-05-26 15:15:08 -0600

    added wood block art

--- entities/objects/wood/wood.gd
+++ entities/objects/wood/wood.gd
@@ -1,7 +1,7 @@
 extends Node2D
 class_name Wood
 
-var wood_item: BlockItem = load("res://entities/objects/wood/wood.tres")
+var wood_item: BlockItem = load("res://entities/objects/woodBlock/wood_block.tres")
 
 func _on_area_2d_area_entered(_area: Area2D) -> void:
 	SignalBus.item_picked.emit(wood_item)
