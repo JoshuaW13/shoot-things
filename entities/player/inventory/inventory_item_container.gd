f7e82385d87cd2ae011f80a96b7f9a977dd095c1
JoshuaW13 <josh.walters@shaw.ca>
2025-05-26 19:46:42 -0600

    added number for consumable items to UI

--- entities/player/inventory/inventory_item_container.gd
+++ entities/player/inventory/inventory_item_container.gd
@@ -5,6 +5,7 @@ signal got_focus(slot_index: int)
 
 @onready var item_texture: TextureRect = %TextureRect
 @onready var texture_button: TextureButton = $TextureButton
+@onready var label: Label = %Label
 
 var is_set: bool = false
 var slot_index: int = -1
@@ -12,6 +13,8 @@ var slot_index: int = -1
 func set_item(item: InventoryItem)->void:
 	item_texture.texture = item.icon
 	is_set = true
+	if item is BlockItem:
+		label.text = str(item.count)
 
 func get_focus()->void:
 	texture_button.grab_focus()
